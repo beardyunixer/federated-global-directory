11ae34f47a433a0ab02b6ebd4d1781e6ff4b3577
redmatrix <redmatrix@redmatrix.me>
2015-11-13 03:06:36 -0800

    hubzilla issue 147 (this is a redmatrix issue)

--- include/diaspora.php
+++ include/diaspora.php
@@ -2749,8 +2749,10 @@ function diaspora_send_relay($item,$owner,$contact,$public_batch = false) {
 	$sublike = false;
 
 
-	if($item['verb'] === ACTIVITY_LIKE && $item['thr_parent']) {
-		$sublike = true;
+	if($item['verb'] === ACTIVITY_LIKE) {
+		if($item['thr_parent'] && ($item['thr_parent'] !== $item['parent_mid'])) {
+			$sublike = true;
+		}
 	}
 
 
