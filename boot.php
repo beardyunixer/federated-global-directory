624e2f345d1dcf745030dea39b874b838d7b14c6
redmatrix <redmatrix@redmatrix.me>
2016-01-23 16:26:41 -0800

    allow option to use exec in proc_run

--- boot.php
+++ boot.php
@@ -1759,8 +1759,12 @@ function proc_run($cmd){
 		$cmd = "cmd /c start \"title\" /D \"$cwd\" /b $cmdline";
 		proc_close(proc_open($cmd, array(), $foo));
 	}
-	else
-		proc_close(proc_open($cmdline ." &", array(), $foo));
+	else {
+		if(get_config('system','proc_open_use_exec')) 
+			exec($cmdline . ' > /dev/null &');
+		else
+			proc_close(proc_open($cmdline ." &", array(), $foo));
+	}
 }
 
 /**
