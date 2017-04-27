78742d981b67669508ee560956686ac2ff4c5642
Thomas Willingham <beardyunixer@beardyunixer.com>
2017-04-27 08:46:32 +0100

    Let Friendica polling work...I think

--- mod/sync.php
+++ mod/sync.php
@@ -1,10 +1,16 @@
 <?php
 
 function sync_content(&$a) {
-// sync/all
+	if (argv(1) == 'pull' && argv(2) == 'all')
+		$SQL_EXTRA = '';
+	if (argv(1) == 'pull' && argv(2) == 'since') {
+		$date = datetime_convert(argv(3));
+		$SQL_EXTRA = "and xchan_updated > " . intval($date);
+	}
+
 	$profiles = array();
 
-  $r = q("select xchan_url from xchan where xchan_network = 'friendica-over-diaspora'");
+  $r = q("select xchan_url from xchan where xchan_network = 'friendica-over-diaspora' $SQL_EXTRA");
 
   foreach($r as $row) $profiles[$row['xchan_url']] = $row['xchan_url'];
   $results = array_values($profiles);
@@ -15,6 +21,6 @@ function sync_content(&$a) {
     'results' => $results
   );
   
-json_return_and_die($data);
-
+	json_return_and_die($data);
 }
+
